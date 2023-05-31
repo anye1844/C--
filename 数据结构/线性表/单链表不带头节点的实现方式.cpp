c3fb5fb8428807b62a88bc8d1f1d41912328dc0b
Anye1844 <anye1844@qq.com>
2023-05-31 15:31:54 +0800

    不带头节点

--- 数据结构/线性表/单链表不带头节点的实现方式.cpp
+++ 数据结构/线性表/单链表不带头节点的实现方式.cpp
@@ -19,6 +19,34 @@ bool Empty(LinkList L) // 判断单链表是否为空
     return (L == NULL); // 如果为空返回true，否则返回false
 }
 
+bool ListInsert(LinkList &L, int i, int e)
+{
+    if (i < 1)
+        return false;
+    if (i = 1)
+    {
+        LNode *p = (LNode *)malloc(sizeof(LNode));
+        p->data = e;
+        p->next = L;
+        L = p;
+        return true;
+    }
+    int j = 1;
+    LNode *p = L;
+    while (L != NULL && j < i - 1)
+    {
+        p = p->next;
+        j++;
+    }
+    if (p == NULL)
+        return false;
+    LNode *s = (LNode *)malloc(sizeof(LNode));
+    s->data = e;
+    s->next = p->next;
+    p->next = s;
+    return true;
+}
+
 int main()
 {
     LinkList L;               // 声明一个指向节点的指针
--- 数据结构/线性表/单链表带头结点的实现方式.cpp
+++ 数据结构/线性表/单链表带头结点的实现方式.cpp
@@ -1,28 +1,62 @@
-#include<iostream>
-#include<stdlib.h>
+#include <iostream>
+#include <stdlib.h>
 using namespace std;
 
-typedef struct LNode{//定义一个单链表数据结构
+typedef struct LNode // 定义一个单链表数据结构
+{
     int data;
     struct LNode *next;
-}LNode, *LinkList;
+} LNode, *LinkList;
 
-bool InitList(LinkList &L){//初始化单链表
-    L = (LNode *)malloc(sizeof(LNode));//分配一个头节点
-    if(L == NULL){//内存不足，分配失败
+bool InitList(LinkList &L) // 初始化单链表
+{
+    L = (LNode *)malloc(sizeof(LNode)); // 分配一个头节点
+    if (L == NULL)
+    { // 内存不足，分配失败
         return false;
     }
-    L->next = NULL;//头节点之后暂时还没有节点，所以暂时指向NULL
+    L->next = NULL; // 头节点之后暂时还没有节点，所以暂时指向NULL
     return true;
 }
 
-bool Empty(LinkList L){//判断单链表是否为空
-    return (L->next == NULL);//如果为空返回true，否则返回false
+bool Empty(LinkList L) // 判断单链表是否为空
+{
+    return (L->next == NULL); // 如果为空返回true，否则返回false
 }
 
-int main(){
-    LinkList L;//声明一个指向节点的指针
-    InitList(L);//初始化单链表
-    cout << Empty(L) << endl;//判断单链表是否为空
+bool ListInsert(LinkList &L, int i, int e) // 在单链表的第i个位置插入元素e
+{
+    if (i < 1)
+    { // i值不合法
+        return false;
+    }
+    LNode *p;  // 声明一个指向节点的指针
+    int j = 0; // 当前p指向的是第几个节点
+    p = L;     // 让p指向头节点，由于头节点是第0个节点，所以p指向第0个节点
+    while (p != NULL && j < i - 1)
+    { // 寻找第i-1个节点
+        p = p->next;
+        j++;
+    }
+    if (p == NULL)
+    { // i值不合法
+        return false;
+    }
+    LNode *s = (LNode *)malloc(sizeof(LNode)); // 分配一个新节点
+    s->data = e;                               // 将数据e赋值给新节点的数据域
+    s->next = p->next;                         // 将新节点的next指针指向第i个节点
+    p->next = s;                               // 将第i-1个节点的next指针指向新节点
+    return true;
+}
+
+int main()
+{
+    LinkList L;               // 声明一个指向节点的指针
+    InitList(L);              // 初始化单链表
+    ListInsert(L, 1, 1);      // 在单链表的第1个位置插入元素1
+    ListInsert(L, 2, 2);      // 在单链表的第1个位置插入元素2
+    ListInsert(L, 3, 3);      // 在单链表的第1个位置插入元素3
+    ListInsert(L, 4, 4);      // 在单链表的第1个位置插入元素4
+    
     return 0;
 }
